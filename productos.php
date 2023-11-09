e501a1f0cf48be5f68c14651b740f471426c3a5e
gilmer3 <gilmergeraaardo@gmail.com>
2023-11-09 14:05:54 -0600

    realizacion de la tabla producto

--- productos.php
+++ productos.php
@@ -18,7 +18,27 @@
     ?>
     <div style="margin: 20px;">
         <br>
-        <table class="table table-dark table-striped">
+        <div class="d-flex justify-content-between">
+            <?php
+                include("./db/conexion.php");
+                $id = $_SESSION['id'];
+                $sql = "SELECT * FROM Usuarios WHERE id = '$id'";
+                $result = mysqli_query($conexion, $sql);
+                $row = mysqli_fetch_array($result);
+                $tipo = $row['nombre'];
+
+                echo "<p>Productos de $tipo</p>";
+
+            ?>
+
+            <a href=""  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Agregar Producto
+            </a>
+
+            
+
+        </div>
+        <table class="table table-light table-striped">
             <thead>
                 <td class="col">Id</td>
                 <td class="col">Nommbre</td>
@@ -26,18 +46,39 @@
                 <td class="col">Categoria</td>
                 <td class="col">Cantidad</td>
                 <td class="col">Precio</td>
+                <td class="col">Acciones</td>
             </thead>
             <tbody>
-                <td>w</td>
-                <td>w</td>
-                <td>w</td>
-                <td>w</td>
-                <td>w</td>
-                <td>w</td>
+                <?php
+                    include("./db/conexion.php");
+                    $id = $_SESSION['id'];
+                    $sql = "SELECT * FROM Productos WHERE id_Vendedor = '$id'";
+                    $result = mysqli_query($conexion, $sql);
+                    while($row = mysqli_fetch_array($result)){
+                        $id = $row['id_producto'];
+                        $nombre = $row['nombre'];
+                        $descripcion = $row['descripcion'];
+                        $categoria = $row['id_categoria'];
+                        $cantidad = $row['cantidad'];
+                        $precio = $row['precio'];
+                        echo "<tr>";
+                        echo "<td>$id</td>";
+                        echo "<td>$nombre</td>";
+                        echo "<td>$descripcion</td>";
+                        echo "<td>$categoria</td>";
+                        echo "<td>$cantidad</td>";
+                        echo "<td>$precio</td>";
+                        echo "<td>";
+                        echo "<a href='./EditarProducto.php?id=$id' class='btn btn-warning'></a>";
+                        echo "<a href='./db/eliminarProducto.php?id=$id' class='btn btn-danger'>Eliminar</a>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                ?>
             </tbody>
         </table>
     </div>
     
     <?php include_once "./public/footer/footer.php"; ?>
 </body>
-</html>
+</html> 
